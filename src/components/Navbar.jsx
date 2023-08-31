735784a36df59b0d7b9cc336aadc9be242cae32d
Shahkzodjon98 <Shahkzodjon98@gmail.ru>
2023-08-31 02:18:43 -0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { hamburger } from "../assets/icons";
+import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+      <nav className='flex justify-between items-center max-container'>
+        <a href='/'>
+          <img
+            src={headerLogo}
+            alt='logo'
+            width={129}
+            height={29}
+            className='m-0 w-[129px] h-[29px]'
+          />
+        </a>
+        <ul className='flex-1 flex justify-center items-center space-x-[150px] max-lg:hidden text-slate-gray '>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className='font-montserrat leading-normal text-lg text-slate-gray hover:text-black font-bold underline'
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+          <a href='/SignIn'>Sign in</a>
+        </div>
+
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
